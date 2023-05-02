a812a8b95d2c6d8d7a639d06771dd364977d27b1
Merek Grimaldus <mordiggian98@gmail.com>
2023-05-02 08:37:35 -0600

    utils.py: switched stop encoding

--- sequence.py
+++ sequence.py
@@ -80,9 +80,6 @@ class Sequence:
     def get_mutated_site_indexes(self):
         return self.__mutated_site_indexes__
 
-    def contains_stop(self):
-        return "X" in self.__amino_acids__
-
     def __str__(self):
         return self.__repr__()
 
@@ -125,17 +122,13 @@ class Sequence:
     def __gen_unique_mutations__(self, num_to_generate, num_mutations, force_mutations=True):
         ret = set()
         while len(ret) < num_to_generate:
-            new_seq = self.__mutate_sequence__(num_mutations, force_mutations)
-            if not new_seq.contains_stop():
-                ret.add(new_seq)
+            ret.add(self.__mutate_sequence__(num_mutations, force_mutations))
         return ret
 
     def __gen_mutations__(self, num_to_generate, num_mutations, force_mutations=True):
         ret = []
-        while len(ret) < num_to_generate:
-            new_seq = self.__mutate_sequence__(num_mutations, force_mutations)
-            if not new_seq.contains_stop():
-                ret.append(new_seq)
+        for i in range(num_to_generate):
+            ret.append(self.__mutate_sequence__(num_mutations, force_mutations))
         return ret
 
     def generate_mutations(self, num_to_generate=100, num_mutations=1, unique=True, force_mutations=True):
@@ -155,34 +148,27 @@ class Sequence:
             other_chars = list(to_combine_with.get_sequence())
         else:
             other_chars = list(to_combine_with)
-        new_seq = None
-        while new_seq is None:
-            new_chars = list(self.get_sequence())
-            for i in range(len(new_chars)):
-                prob = random.random()
-                should_combine = prob > probability
-                if should_combine:
-                    new_chars[i] = other_chars[i]
-            possible_new = Sequence(''.join(new_chars), self.__base_sequence__, self.__original_amino_acids__)
-            if not possible_new.contains_stop():
-                new_seq = possible_new
-        return new_seq
+        new_chars = list(self.get_sequence())
+        for i in range(len(new_chars)):
+            prob = random.random()
+            should_combine = prob > probability
+            if should_combine:
+                new_chars[i] = other_chars[i]
+        return Sequence(''.join(new_chars), self.__base_sequence__, self.__original_amino_acids__)
 
     def force_combine(self, to_combine_with, number_of_sites_to_swap):
         if isinstance(to_combine_with, Sequence):
             other_chars = list(to_combine_with.get_sequence())
         else:
             other_chars = list(to_combine_with)
-        new_seq = None
-        while new_seq is None:
-            new_chars = list(self.get_sequence())
-            sites_to_swap = self.generate_unique_randoms(len(self.__sequence__) - 1, number_of_sites_to_swap)
-            for site_to_swap in sites_to_swap:
-                new_chars[site_to_swap] = other_chars[site_to_swap]
-            possible_new = Sequence(''.join(new_chars), self.__base_sequence__, self.__original_amino_acids__)
-            if not possible_new.contains_stop():
-                new_seq = possible_new
-        return new_seq
+        new_chars = list(self.get_sequence())
+        sites_to_swap = self.generate_unique_randoms(len(self.__sequence__) - 1, number_of_sites_to_swap)
+        for site_to_swap in sites_to_swap:
+            new_chars[site_to_swap] = other_chars[site_to_swap]
+        return Sequence(''.join(new_chars), self.__base_sequence__, self.__original_amino_acids__)
+
+    def contains_stop(self):
+        return "X" in self.__amino_acids__ or "*" in self.__amino_acids__
 
 
 def load_old_pickle(file_name, original_sequence=load_sequence()):
